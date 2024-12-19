ef4833aad14f60066a566e862b744a57e5333d2f
rizal-creator <rizalbusinnes99max@gmail.com>
2024-12-19 23:37:16 +0800

    perubahan

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pixpro/', // Ensure this is correct, corresponding to your repository name
+  base: '/PixPro/', // Ensure this is correct, corresponding to your repository name
 })
